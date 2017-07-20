379a589e66c3ce2b4a68123cb401638942f4c777
Alexandra Roth <alexandra.roth@vaynermedia.com>
2017-07-20 16:16:38 -0400

    heading in another direction to learn express will be back soon exclamationpoint stupid bash.

--- routes/main.js
+++ routes/main.js
@@ -4,7 +4,13 @@ var express     = require('express')
 
 router.route('/')
     .get(function(req, res) {
-        res.render('index', { title: 'VM Dinner Boilerplate', word: 'alex did a thing'});
+        res.render('index', { title: 'Alex did a thing'});
+    })
+;
+
+router.route('/powerlifting')
+    .get(function(req, res) {
+        res.render('power', { title: 'Alex lifts heavy things'});
     })
 ;
 
